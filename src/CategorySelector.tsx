2ddae61526b2fd6c8dea4c7aa478e3a3c5091b6f
Thomas Faddegon <tomfaddegon@gmail.com>
2024-02-06 11:30:28 -0500

    added additional styles to category selector buttons

--- src/CategorySelector.tsx
+++ src/CategorySelector.tsx
@@ -11,14 +11,14 @@ const CategorySelector: React.FC<CategorySelectorProps> = ({
   currentCategory,
   handleCategoryChange,
 }) => {
-  const border =
+  const active =
     currentCategory === category
-      ? "border-2 border-purple-500"
+      ? "border-t-2 border-l-2 border-r-2 border-white bg-purple-500"
       : "border-2 border-white";
 
   return (
     <div
-      className={`${border} px-2 py-2`}
+      className={`${active} px-2 py-2 cursor-pointer hover:bg-white hover:text-black`}
       onClick={() => handleCategoryChange(category)}
     >
       {category}
