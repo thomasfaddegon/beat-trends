14eb15e7fae138c429730dc8bceab9833b867c3a
Thomas Faddegon <tomfaddegon@gmail.com>
2024-02-29 21:56:09 -0500

    readded extension to fix build error

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from "react";
-import Graph from "./Graph";
+import Graph from "./Graph.tsx";
 import "./App.css";
 import { DataSeries } from "./types";
 import CategorySelector from "./CategorySelector";
